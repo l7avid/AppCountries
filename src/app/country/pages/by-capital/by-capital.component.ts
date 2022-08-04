89a006fddfd859a18cc5f1672c28ddaccdcf0a7b
David Reina <davidereina@hotmail.com>
2022-08-04 14:11:18 -0500

    Working on sidebar

---
+++ src/app/country/pages/by-capital/by-capital.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-by-capital',
+  templateUrl: './by-capital.component.html'
+})
+export class ByCapitalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/country/pages/single-country/single-country.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-country',
+  templateUrl: './single-country.component.html'
+})
+export class SingleCountryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
