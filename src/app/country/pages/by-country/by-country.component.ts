4d3716036404eb8f222ba22312c5816e7d4a8f89
David Reina <davidereina@hotmail.com>
2022-08-25 12:07:03 -0500

    suggestions in the country input implemented

--- src/app/country/pages/by-country/by-country.component.ts
+++ src/app/country/pages/by-country/by-country.component.ts
@@ -4,17 +4,25 @@ import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-by-country',
-  templateUrl: './by-country.component.html'
+  templateUrl: './by-country.component.html',
+  styles: [`
+  li {
+    cursor: pointer;
+  }
+  `
+  ]
 })
 export class ByCountryComponent {
 
   element: string = ""
   errorExist: boolean = false;
   countries:  Country[] = [];
+  suggestedCountries: Country[] = [];
+  showSuggestions: boolean = false;
 
   constructor( private countryService: CountryService ) { }
 
-  search( element: string ){
+  search(element: string){
     this.errorExist = false;
     this.element = element;
 
@@ -29,8 +37,13 @@ export class ByCountryComponent {
   }
 
   suggestions(element: string){
+    this.showSuggestions = true;
     this.errorExist = false;
-    
+    this.element = element;
+    this.countryService.searchCountry(element).subscribe((countries) =>
+      this.suggestedCountries = countries.splice(0,4),
+      (err) => this.suggestedCountries = []
+    )
   }
 
 }
